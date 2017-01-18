842c96e9fd227e2b04befa4a81c82e76d784aac1
Saul Johnson <saul.a.johnson@gmail.com>
2017-01-18 15:08:11 +0000

    Add init.php initialisation script

---
+++ web/init.php
@@ -0,0 +1,11 @@
+<?php
+
+// Common functions, and database functions.
+require_once 'functions.php';
+require_once 'db_functions.php';
+
+// Report all errors.
+error_reporting(E_ALL);
+
+// Start session.
+session_start();
