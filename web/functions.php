0bab3a7fc448dee99dcf66c79cfc43b24f3bac9d
Saul Johnson <saul.a.johnson@gmail.com>
2017-01-18 16:17:16 +0000

    Add authentication and redirection functions

--- web/functions.php
+++ web/functions.php
@@ -12,3 +12,59 @@ function tryGetRequestValue($key) {
     }
     return null;
 }
+
+/**
+ * Tries to get a value from the session super-global, returning null if unset.
+ *
+ * @param string $key   the key of the value to return
+ * @return null|mixed
+ */
+function tryGetSessionValue($key) {
+    if (isset($_SESSION[$key])) {
+        return $_SESSION[$key];
+    }
+    return null;
+}
+
+/**
+ * Returns true if the user is authenticated, otherwise returns false.
+ *
+ * @return bool
+ */
+function isAuthenticated() {
+    $authenticated = tryGetSessionValue('authed');
+    return $authenticated === true;
+}
+
+/**
+ * Redirects the user to the given URL and ends the request.
+ *
+ * @param string $url   the URL to redirect to
+ */
+function redirect($url) {
+    header("Location: $url");
+    die();
+}
+
+/**
+ * Redirects the user to the restricted page.
+ */
+function redirectToRestrictedPage() {
+    redirect('/restricted.php');
+}
+
+/**
+ * Redirects the user to the login page.
+ */
+function redirectToLogin() {
+    redirect('/login.php');
+}
+
+/**
+ * Redirects the user to the login page if they're not authenticated.
+ */
+function protectPage() {
+    if (!isAuthenticated()) {
+        redirectToLogin();
+    }
+}
